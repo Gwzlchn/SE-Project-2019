7fb5ac5fc0940b2141230e2240947e438b3adbfe
otakuxiang <455953532@qq.com>
2019-06-16 18:40:31 +0800

    python django project

---
+++ inspage/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InspageConfig(AppConfig):
+    name = 'inspage'
---
+++ sab/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SabConfig(AppConfig):
+    name = 'sab'
