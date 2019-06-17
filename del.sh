e5da09b004464c08153e48d6237cb6b5e18f7ac7
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-06-17 11:28:44 +0800

    del.sh

--- del.sh
+++ del.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 git filter-branch --force --index-filter  'git rm --cached -r --ignore-unmatch \
-'document/Spring in Action'  --prune-empty --tag-name-filter cat -- --all
+document/Spring in Action'  --prune-empty --tag-name-filter cat -- --all
