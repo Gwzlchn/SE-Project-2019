ba7caf86a94fa52bafb103c8ce42c9c4e4ac2ffc
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-06-17 18:35:10 +0800

    article

--- apps/fondamental/article/urls.py
+++ apps/fondamental/article/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
     #写文章
     path('article-create/', views.article_create, name='article_create'),
+    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+# 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
 ]
