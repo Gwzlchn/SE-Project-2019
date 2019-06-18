fa0a795266ba34da01b0a7a4104ffd9560baf79d
otakuxiang <455953532@qq.com>
2019-06-18 23:03:09 +0800

    python django project

--- apps/userHome/models.py
+++ apps/userHome/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from apps.User.models import POS
+from apps.User.models import Parent
 from apps.course.models import Course
 class tlesson(models.Model):
-    person = models.ForeignKey(POS,on_delete=models.CASCADE,verbose_name='试听用户')
+    person = models.ForeignKey(Parent,on_delete=models.CASCADE,verbose_name='试听用户')
     lesson = models.ForeignKey(Course,on_delete=models.CASCADE,verbose_name='视听课程')
