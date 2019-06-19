705f275756be177483ad143075288a8b3ae4433b
edenyann <edenyann@users.noreply.github.com>
2019-06-19 14:34:30 +0800

    init template 2

---
+++ templates2/assets/js/contact-form-script.js
@@ -0,0 +1,62 @@
+/*==============================================================*/
+// EduNeed Contact Form  JS
+/*==============================================================*/
+(function ($) {
+    "use strict"; // Start of use strict
+
+$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "Did you fill in the form properly?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var msg_subject = $("#msg_subject").val();
+    var message = $("#message").val();
+
+
+    $.ajax({
+        type: "POST",
+        url: "assets/php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+
+function formSuccess(){
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Message Submitted!")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h5 text-center tada animated text-success mt-3";
+    } else {
+        var msgClasses = "h5 text-center text-danger mt-3";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+}(jQuery)); // End of use strict
