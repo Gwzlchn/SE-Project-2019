d575593d209ab40707fd7ccde4937a0051aa7fc5
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-06-26 22:17:07 +0800

    json replace test

--- apps/course/views.py
+++ apps/course/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 from .models import Course_Base
 
 # Create your views here.
@@ -6,8 +7,10 @@ def Course_Index(request):
     return render(request,"courses.html")
 
 def Single_Course_Index(request,course_id):
+    return render(request,"single_course_test.html")
 
-    return render(request,"single_course.html")
+def Single_Course_Teacher(request,course_id):
+    return JsonResponse({'Teacher':'AAAAA'})
 
 def Scoring_Course(request):
     pass
